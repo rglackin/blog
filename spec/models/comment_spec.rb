0a3e3a7067ccaab0c9a2afdbdd4c7a01d3b66a04
Ruairí Glackin <ruairiglackin@gmail.com>
2024-07-02 11:37:09 +0100

    added comment model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures [:users,:articles, :comments]
+  describe 'validations' do
+    it "is valid with valid attributes" do
+      comment = Comment.new(body: "Test",user: users(:one), article: articles(:one))
+      expect(comment).to be_valid
+    end
+    it "is not valid without a body" do
+      comment = comments(:one)
+      comment.body = nil
+      expect(comment).to_not be_valid
+    end
+    it "is not valid without a user" do
+      comment = comments(:one)
+      comment.user = nil
+      expect(comment).to_not be_valid
+    end
+    it "is not valid without an article" do
+      comment = comments(:one)
+      comment.article = nil
+      expect(comment).to_not be_valid
+    end
+  end
+  describe 'associations' do
+    it "belongs to a user" do
+      comment = comments(:one)
+      user = users(:one)
+      expect(comment.user).to eq(user)
+    end
+    it "belongs to an article" do
+      comment = comments(:one)
+      article = articles(:one)
+      expect(comment.article).to eq(article)
+    end
+  end
 end
