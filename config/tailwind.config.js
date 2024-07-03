79578d64bd443919054940c1a494860e58ca3f0f
Ruairí Glackin <ruairiglackin@gmail.com>
2024-07-03 11:11:11 +0100

    build fix

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
@@ -56,7 +55,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-    require('flowbite/plugin')
   ],
   
   
