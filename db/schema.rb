e604a76ff0a06590f76030c07a9738c27c757768
Ruairí Glackin <ruairiglackin@gmail.com>
2024-07-04 15:01:38 +0100

    added bio to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_092710) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_04_135017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_092710) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.boolean "admin", default: false
+    t.text "bio", default: "No bio yet"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
