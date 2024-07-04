2b39f3acdc5ac0719f491701806dc3c6be06bd33
Ruairí Glackin <ruairiglackin@gmail.com>
2024-07-04 12:43:45 +0100

    created and implemented article list partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
+        @pagy ,@articles = pagy(policy_scope(@user.articles), items:12)
+        @total_articles = @pagy.count
     end
 end
