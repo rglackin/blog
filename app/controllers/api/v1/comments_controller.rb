cd0a594c9f38ad31da584c2b6e0a17c21bf26392
Ruairí Glackin <ruairiglackin@gmail.com>
2024-07-09 15:49:30 +0100

    fixed CSRF error on comments api

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CommentsController < ApplicationController
     before_action :set_article
     before_action :set_comment, only: [:destroy]
+    skip_before_action :verify_authenticity_token
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
     def create
         authorize @comment
